83501d0e9601fb862684057aa7ab56e287957155
Arthur Zakirov <zakirofartur@gmail.com>
2024-09-09 22:16:01 +0500

    Init

---
+++ HW_42_Arthur_Zakirov/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+const int port = 5000;
+const string stopMessage = "Stop";
+
+RunClient();
+
+void RunClient()
+{
+    try
+    {
+        while (ClientWorker()) {}
+       
+        Console.WriteLine("Client Finished.");
+    }
+    catch (Exception e)
+    {
+        Console.WriteLine(e);
+        throw;
+    }
+}
+
+bool ClientWorker()
+{
+    var ipAddress = IPAddress.Loopback;
+    var endpoint = new IPEndPoint(ipAddress, port);
+
+    using var sender = new Socket(
+        ipAddress.AddressFamily, 
+        SocketType.Stream, 
+        ProtocolType.Tcp);
+
+    sender.Connect(endpoint);
+    Console.WriteLine($"Client Connected: {sender.RemoteEndPoint}");
+
+    Console.WriteLine("Read message: ");
+    var msg = Console.ReadLine();
+
+    var bytes = Encoding.UTF8.GetBytes(msg);
+    sender.Send(bytes);
+
+    var buffer = new byte[1024];
+    var receivedBytes = sender.Receive(buffer);
+    
+    Console.WriteLine($"Received: {receivedBytes}");
+    var response = Encoding.UTF8.GetString(
+        buffer,
+        0,
+        receivedBytes);
+
+    Console.WriteLine($"Server responded: {response}");
+    sender.Shutdown(SocketShutdown.Both);
+    sender.Close();
+
+    return !response.Equals(stopMessage);
+}
---
+++ Server/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+const int serverPort = 5000;
+const string stopMessage = "Stop";
+
+try
+{
+    MainWorker();
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+    throw;
+}
+
+
+void MainWorker()
+{
+    var ipAddress = IPAddress.Loopback;
+    var endPoint = new IPEndPoint(ipAddress, serverPort);
+
+    using var listener = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+    listener.Bind(endPoint);
+    listener.Listen(20);
+
+    while (true)
+    {
+        Console.WriteLine($"Waiting for connection for {listener.LocalEndPoint}");
+    
+        using var handler = listener.Accept();
+
+        var bytes = new byte[1024];
+        var receivedBytes = handler.Receive(bytes);
+
+        string request = Encoding.UTF8.GetString(
+            bytes,
+            0,
+            receivedBytes);
+        
+        Console.WriteLine($"Request: {request}");
+
+        var response = $"Receive request with length {request.Length}";
+        var responseBytes = Encoding.UTF8.GetBytes(response);
+        handler.Send((responseBytes));
+    
+        handler.Shutdown(SocketShutdown.Both);
+        handler.Close();
+        if (request.Equals(stopMessage))
+        {
+            break;
+        }
+    }
+}
